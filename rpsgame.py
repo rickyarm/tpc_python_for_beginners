b2452b2b1a613a0444300e25d52c68b092ac606d
rickyarm <rick.armanino@gmail.com>
2023-07-23 13:19:57 -0400

    Updated Rock Paper Scissors Game

---
+++ rpsgame.py
@@ -0,0 +1,7 @@
+
+print("----------------------------------------------------")
+print("     Rock Paper Scissors v1.0 Game ")
+print("----------------------------------------------------")
+
+player_1 = input(" Player 1 Name: ")
+player_2 = input(" Palyer 2 Name: ")
