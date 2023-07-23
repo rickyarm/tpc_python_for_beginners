799c9825720e7a3409cb7031780929c0fa9986f4
rickyarm <rick.armanino@gmail.com>
2023-07-23 11:38:07 -0400

    Updated guessing game

--- guessinggame.py
+++ guessinggame.py
@@ -5,5 +5,19 @@ print("       New M&M Guessing game!")
 print("---------------------------")
 
 mm_count = random.randint(0, 10000)
+attempts_limit = 5
+attempts = 0
 
-print(mm_count)
+while attempts < attempts_limit:
+    guess_text = input("How many M&Ms are in the jar?  ")
+    guess = int(guess_text)
+
+    if mm_count == guess:
+        print("You Win")
+        break
+    elif mm_count > guess:
+        print("Too Low")
+    elif mm_count < guess:
+        print("Too High")
+
+    attempts += 1
