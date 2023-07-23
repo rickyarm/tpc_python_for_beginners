4f4c1abed568e6b80731e53fb9b1383978b147c1
rickyarm <rick.armanino@gmail.com>
2023-07-23 11:29:13 -0400

    Added new file guessinggame.py

---
+++ guessinggame.py
@@ -0,0 +1,9 @@
+import random
+
+print("---------------------------")
+print("       New M&M Guessing game!")
+print("---------------------------")
+
+mm_count = random.randint(0, 10000)
+
+print(mm_count)
